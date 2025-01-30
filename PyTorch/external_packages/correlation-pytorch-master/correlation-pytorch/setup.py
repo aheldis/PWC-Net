0cadb444b0b030ebce3ae328573c564bea95ce7d
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2025-01-29 17:11:36 -0800

    debug

--- PyTorch/external_packages/correlation-pytorch-master/correlation-pytorch/setup.py
+++ PyTorch/external_packages/correlation-pytorch-master/correlation-pytorch/setup.py
@@ -1,8 +1,37 @@
+# #!/usr/bin/env python
+
+# import os
+
+# from setuptools import setup, find_packages
+
+# this_file = os.path.dirname(__file__)
+
+# setup(
+#     name="correlation_package",
+#     version="0.1",
+#     description="Correlation layer from FlowNetC",
+#     url="https://github.com/jbarker-nvidia/pytorch-correlation",
+#     author="Jon Barker",
+#     author_email="jbarker@nvidia.com",
+#     # Require cffi
+#     install_requires=["cffi>=1.0.0"],
+#     setup_requires=["cffi>=1.0.0"],
+#     # Exclude the build files.
+#     packages=find_packages(exclude=["build"]),
+#     # Package where to put the extensions. Has to be a prefix of build.py
+#     ext_package="",
+#     # Extensions to compile
+#     cffi_modules=[
+#         os.path.join(this_file, "build.py:ffi")
+#     ],
+# )
+
+
 #!/usr/bin/env python
 
 import os
-
 from setuptools import setup, find_packages
+from torch.utils.cpp_extension import CppExtension, BuildExtension
 
 this_file = os.path.dirname(__file__)
 
@@ -13,15 +42,13 @@ setup(
     url="https://github.com/jbarker-nvidia/pytorch-correlation",
     author="Jon Barker",
     author_email="jbarker@nvidia.com",
-    # Require cffi
-    install_requires=["cffi>=1.0.0"],
-    setup_requires=["cffi>=1.0.0"],
-    # Exclude the build files.
+    install_requires=["torch>=1.0.0"],  # Add PyTorch as a dependency
+    setup_requires=["torch>=1.0.0"],
     packages=find_packages(exclude=["build"]),
-    # Package where to put the extensions. Has to be a prefix of build.py
-    ext_package="",
-    # Extensions to compile
-    cffi_modules=[
-        os.path.join(this_file, "build.py:ffi")
+    ext_modules=[
+        CppExtension(
+            name="",  # Name of the compiled extension
+        )
     ],
+    cmdclass={"build_ext": BuildExtension},
 )
